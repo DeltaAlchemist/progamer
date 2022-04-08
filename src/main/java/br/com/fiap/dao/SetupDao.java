b20b53ff8fcc25ee25138be3c7c13411f4129ab4
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-08 08:30:24 -0300

    feat: add setupdao

--- src/main/java/br/com/fiap/dao/SetupDao.java
+++ src/main/java/br/com/fiap/dao/SetupDao.java
@@ -1,12 +1,35 @@
 package br.com.fiap.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 import br.com.fiap.model.Setup;
 
 public class SetupDao {
 
 	public void insert(Setup setup) {
-		// TODO Auto-generated method stub
-		
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("progamer-persistence-unit");
+		EntityManager manager = factory.createEntityManager();
+
+		manager.getTransaction().begin();
+		manager.persist(setup);
+		manager.getTransaction().commit();
+
+		manager.clear();
+	}
+
+	public List<Setup> list() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("progamer-persistence-unit");
+		EntityManager manager = factory.createEntityManager();
+
+		TypedQuery<Setup> query = manager.createQuery("SELECT s from Setup s", Setup.class);
+
+		return query.getResultList();
 	}
 
 }
